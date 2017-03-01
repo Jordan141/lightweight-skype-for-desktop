d047b829345c738495c64f3d3a78eaeade310e71
Michael Kurowski <Byamarro@gmail.com>
2017-03-01 01:07:02 +0100

    [test][package][main] Example test

--- src/test/test.js
+++ src/test/test.js
@@ -1,4 +1,5 @@
 var assert = require('assert');
+let thisIsATest = require('../main.js').thisIsATest;
 describe('Array', function() {
 	describe('#indexOf()', function() {
 		it('should return -1 when the value is not present', function() {
@@ -6,3 +7,11 @@ describe('Array', function() {
 		});
 	});
 });
+describe('thisIsATest', () => {
+	it('is an object', () => {
+		if (typeof thisIsATest !== 'object'){
+			console.log('thisIsATest', thisIsATest)
+			throw Error('thisIsATest is not an object')
+		}
+	})
+})
